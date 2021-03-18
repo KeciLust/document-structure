765ed10584b465c6da46b2b2dad9cd9b9f4998fc
Denis Nikitin <dinya.nikitin@gmail.com>
2021-03-18 10:00:15 +0300

    change `tooltip` Finish

--- tooltip/task.js
+++ tooltip/task.js
@@ -4,13 +4,16 @@ tooltip.classList.add(`tooltip`);
 
 help.forEach((el, i) => {
   el.addEventListener(`click`, (e) => {
-    tooltip.classList.remove(`tooltip_active`);
-    el.insertAdjacentElement(`afterend`, tooltip);
-    tooltip.classList.toggle(`tooltip_active`);
-    tooltip.textContent = el.title;
-    tooltip.style.left = `${el.getBoundingClientRect().left}px`;
-    tooltip.style.top = `${el.getBoundingClientRect().top + 15}px`;
-    e.preventDefault();
-
+    if (el.nextSibling === tooltip) {
+      tooltip.classList.toggle(`tooltip_active`);
+    } else {
+      el.insertAdjacentElement(`afterend`, tooltip);
+      
+      tooltip.classList.add(`tooltip_active`);
+      tooltip.textContent = el.title;
+      tooltip.style.left = `${el.getBoundingClientRect().left}px`;
+      tooltip.style.top = `${el.getBoundingClientRect().top + 15}px`;
+      e.preventDefault();
+    }
   });
 });
